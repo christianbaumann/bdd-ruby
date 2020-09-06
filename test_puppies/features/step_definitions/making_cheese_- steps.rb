c9e1f929b6cc2c7cf25c7698546d67e1f89366c8
ChrissBaumann <chriss713@gmail.com>
2020-09-06 14:19:09 +0200

    Added step definitions

---
+++ test_puppies/features/step_definitions/making_cheese_- steps.rb
@@ -0,0 +1,12 @@
+Given('I have no cheese') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I press the make cheese button') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should have {int} piece of cheese') do |int|
+  # Then('I should have {float} piece of cheese') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
