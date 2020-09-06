9668aecd02eaa959bd4dff4a0ab02be6c787a92d
ChrissBaumann <chriss713@gmail.com>
2020-09-06 20:45:39 +0200

    Refactoring

--- test_puppies/features/step_definitions/adopting_puppy_steps.rb.rb
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb.rb
@@ -1,64 +1,55 @@
-Given(/^I am on the puppy adoption site$/) do
-  @browser.goto "http://puppies.herokuapp.com"
-  @home = HomePage.new(@browser)
+Given /^I am on the puppy adoption site$/ do
+  visit(HomePage)
 end
 
-When(/^I click the first View Details button$/) do
-  @home.select_puppy_number 1
-  @details = DetailsPage.new(@browser)
+When /^I click the View Details button for "([^"]*)"$/ do |name|
+  on(HomePage).select_puppy name
 end
 
-And(/^I click the Adopt Me button$/) do
-  @browser.button(value: 'Adopt Me!').click
-  @cart = ShoppingCartPage.new(@browser)
+When /^I click the Adopt Me button$/ do
+  on(DetailsPage).add_to_cart
 end
 
-And(/^I click the Complete the Adoption button$/) do
-  @cart.proceed_to_checkout
-  @checkout = CheckoutPage.new(@browser)
+When /^I click the Complete the Adoption button$/ do
+  on(ShoppingCartPage).proceed_to_checkout
 end
 
-And(/^I enter "([^"]*)" in the name field$/) do |name|
-  @checkout.name = name
+When /^I click the Adopt Another Puppy button$/ do
+  on(ShoppingCartPage).continue_shopping
 end
 
-And(/^I enter "([^"]*)" in the address field$/) do |address|
-  @checkout.address = address
+When /^I enter "([^"]*)" in the name field$/ do |name|
+  on(CheckoutPage).name = name
 end
 
-And(/^I enter "([^"]*)" in the email field$/) do |email|
-  @browser.text_field(id: 'order_email').set(email)
+When /^I enter "([^"]*)" in the address field$/ do |address|
+  on(CheckoutPage).address = address
 end
 
-And(/^I select "([^"]*)" from the pay with dropdown$/) do |pay_type|
-  @checkout.pay_type = pay_type
+When /^I enter "([^"]*)" in the email field$/ do |email|
+  on(CheckoutPage).email = email
 end
 
-And(/^I click the Place Order button$/) do
-  @checkout.place_order
+When /^I select "([^"]*)" from the pay with dropdown$/ do |pay_type|
+  on(CheckoutPage).pay_type = pay_type
 end
 
-Then(/^I should see "([^"]*)"$/) do |expected|
-  expect(@browser.text).to include expected
+When /^I click the Place Order button$/ do
+  on(CheckoutPage).place_order
 end
 
-And(/^I click the Adopt Another Puppy button$/) do
-  @cart.continue_shopping
-end
-
-And(/^I click the second View Details button$/) do
-  @home.select_puppy_number 2
-  @details = DetailsPage.new(@browser)
+Then /^I should see "([^"]*)"$/ do |expected|
+  expect(@current_page.text).to include expected
 end
 
 Then /^I should see "([^"]*)" as the name for line item (\d+)$/ do |name, line_item|
-  expect(@cart.name_for_line_item(line_item.to_i)).to include name
+  expect(on(ShoppingCartPage).name_for_line_item(line_item)).to include name
 end
 
 When /^I should see "([^"]*)" as the subtotal for line item (\d+)$/ do |subtotal, line_item|
-  expect(@cart.subtotal_for_line_item(line_item.to_i)).to eql subtotal
+  expect(on(ShoppingCartPage).subtotal_for_line_item(line_item)).to eql subtotal
 end
 
-When(/^I should see "([^"]*)" as the cart total$/) do |total|
-  expect(@cart.cart_total).to eql total
+When /^I should see "([^"]*)" as the cart total$/ do |total|
+  expect(on(ShoppingCartPage).cart_total).to eql total
 end
