22400df6495d37ec87da761571070940cbf44d7d
ChrissBaumann <chriss713@gmail.com>
2020-09-06 14:23:36 +0200

    Added "adopting puppies" feature and step dummies

---
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb.rb
@@ -0,0 +1,39 @@
+Given(/^I am on the puppy adoption site$/) do
+  pending
+end
+
+When(/^I click the View Details button$/) do
+  pending
+end
+
+And(/^I click the Adopt Me button$/) do
+  pending
+end
+
+And(/^I click the Complete the Adoption button$/) do
+  pending
+end
+
+And(/^I enter "([^"]*)" in the name field$/) do |arg|
+  pending
+end
+
+And(/^I enter "([^"]*)" in the address field$/) do |arg|
+  pending
+end
+
+And(/^I enter "([^"]*)" in the email field$/) do |arg|
+  pending
+end
+
+And(/^I select "([^"]*)" from the pay with dropdown$/) do |arg|
+  pending
+end
+
+And(/^I click the Place Order button$/) do
+  pending
+end
+
+Then(/^I should see "([^"]*)"$/) do |arg|
+  pending
+end
