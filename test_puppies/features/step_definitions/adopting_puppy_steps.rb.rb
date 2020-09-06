098dc8f3b40ed8b37b10640e9ec0dcbcb865a1d9
ChrissBaumann <chriss713@gmail.com>
2020-09-06 14:27:27 +0200

    Adding real steps for adopting a puppy

--- test_puppies/features/step_definitions/adopting_puppy_steps.rb.rb
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb.rb
@@ -1,39 +1,39 @@
 Given(/^I am on the puppy adoption site$/) do
-  pending
+  @browser.goto "http://puppies.herokuapp.com"
 end
 
 When(/^I click the View Details button$/) do
-  pending
+  @browser.button(value: 'View Details').click
 end
 
 And(/^I click the Adopt Me button$/) do
-  pending
+  @browser.button(value: 'Adopt Me!').click
 end
 
 And(/^I click the Complete the Adoption button$/) do
-  pending
+  @browser.button(value: 'Complete the Adoption').click
 end
 
-And(/^I enter "([^"]*)" in the name field$/) do |arg|
-  pending
+And(/^I enter "([^"]*)" in the name field$/) do |name|
+  @browser.text_field(id: 'order_name').set(name)
 end
 
-And(/^I enter "([^"]*)" in the address field$/) do |arg|
-  pending
+And(/^I enter "([^"]*)" in the address field$/) do |address|
+  @browser.textarea(id: 'order_address').set(address)
 end
 
-And(/^I enter "([^"]*)" in the email field$/) do |arg|
-  pending
+And(/^I enter "([^"]*)" in the email field$/) do |email|
+  @browser.text_field(id: 'order_email').set(email)
 end
 
-And(/^I select "([^"]*)" from the pay with dropdown$/) do |arg|
-  pending
+And(/^I select "([^"]*)" from the pay with dropdown$/) do |pay_type|
+  @browser.select_list(id: 'order_pay_type').select(pay_type)
 end
 
 And(/^I click the Place Order button$/) do
-  pending
+  @browser.button(value: 'Place Order').click
 end
 
-Then(/^I should see "([^"]*)"$/) do |arg|
-  pending
+Then(/^I should see "([^"]*)"$/) do |text|
+  fail unless @browser.text.include? text
 end
