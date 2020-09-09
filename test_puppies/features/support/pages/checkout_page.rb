6a666cae828c9d306574c4d28deb110512613690
ChrissBaumann <chriss713@gmail.com>
2020-09-09 20:33:59 +0200

    Using "default data"

--- test_puppies/features/support/pages/checkout_page.rb
+++ test_puppies/features/support/pages/checkout_page.rb
@@ -1,13 +1,21 @@
 class CheckoutPage
   include PageObject
 
+  DEFAULT_DATA = {
+      :name => 'cheezy',
+      :address => '123 Main Street',
+      :email => 'cheezy@example.com',
+      :pay_type => 'Purchase order'
+  }
+
   text_field(:name, id: "order_name")
   text_area(:address, id: "order_address")
   text_field(:email, id: "order_email")
   select_list(:pay_type, id: "order_pay_type")
   button(:place_order, value: "Place Order")
 
-  def checkout(data)
+  def checkout(data = {})
+    data = DEFAULT_DATA.merge(data)
     self.name = data[:name]
     self.address = data[:address]
     self.email = data[:email]
